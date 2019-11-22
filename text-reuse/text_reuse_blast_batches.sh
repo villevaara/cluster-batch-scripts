5f83d541559698441ce735d809e07108dfd55e7b
Ville Vaara <villepvaara@gmail.com>
2019-11-22 18:05:25 +0200

    First real blast run test

--- text-reuse/text_reuse_blast_batches.sh
+++ text-reuse/text_reuse_blast_batches.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=textReuseECCOdataPrep
+#SBATCH --job-name=textReuseECCO
 #SBATCH --account=Project_2000230
 #SBATCH --partition=small
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=256G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=256G
 #SBATCH --output=std.out
 #SBATCH --error=std.err
 #SBATCH --mail-type=ALL
@@ -20,4 +20,4 @@ source venv/bin/activate
 pip install -r requirements.txt
 cd /scratch/project_2000230/txt_reuse/blast_ecco/code/work
 export PATH="/scratch/project_2000230/txt_reuse/ncbi-blast-2.5.0+-src/c++/ReleaseMT/bin:$PATH"
-srun python data_preparer.py --data_location="/scratch/project_2000230/txt_reuse/chunks_for_blast/" --output_folder="/scratch/project_2000230/txt_reuse/blast_work/" --language="ENG" --threads=1
+srun python blast_batches.py  --output_folder="/scratch/project_2000230/txt_reuse/blast_work/" --batch_folder="/scratch/project_2000230/txt_reuse/blast_work/data_out" --threads=4 --text_count=5203431 --qpi=100 --iter=0 --e_value=0.000000001
---
+++ text-reuse/usage-stats/text_reuse_data_preparer.md
@@ -0,0 +1,13 @@
+Job ID: 482980
+Cluster: puhti
+User/Group: vvaara/vvaara
+State: COMPLETED (exit code 0)
+Cores: 1
+CPU Utilized: 06:45:18
+CPU Efficiency: 71.29% of 09:28:29 core-walltime
+Job Wall-clock time: 09:28:29
+Memory Utilized: 29.31 GB
+Memory Efficiency: 11.45% of 256.00 GB
+Job consumed 252.03 CSC billing units based on following used resources
+CPU BU: 9.47
+Mem BU: 242.55
