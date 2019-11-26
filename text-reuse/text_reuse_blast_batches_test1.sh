7f698fb17fe90e2658deb860dc9332952d0dddc4
Ville Vaara <villepvaara@gmail.com>
2019-11-26 13:42:04 +0200

    Update test runs.

--- text-reuse/text_reuse_blast_batches_test1.sh
+++ text-reuse/text_reuse_blast_batches_test1.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=40G
 #SBATCH --output=std.out
 #SBATCH --error=std.err
 #SBATCH --mail-type=ALL
@@ -14,10 +14,7 @@
 
 cd /scratch/project_2000230/txt_reuse/blast_ecco/
 module load python-data/3.7.3-1
-rm -r venv
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt --user
 cd /scratch/project_2000230/txt_reuse/blast_ecco/code/work
 export PATH="/scratch/project_2000230/txt_reuse/ncbi-blast-2.5.0+-src/c++/ReleaseMT/bin:$PATH"
-srun python blast_batches.py  --output_folder="/scratch/project_2000230/txt_reuse/blast_work/" --batch_folder="/scratch/project_2000230/txt_reuse/blast_work/data_out_e0_0000000000001_qpi10_i0_threads8" --threads=8 --text_count=5203431 --qpi=10 --iter=0 --e_value=0.0000000000001
+srun python blast_batches.py  --output_folder="/scratch/project_2000230/txt_reuse/blast_work_full_testsets/set1" --batch_folder="/scratch/project_2000230/txt_reuse/blast_work_full_testsets/set1/data_out_e0_00000000001_qpi10_i0_threads20" --threads=20 --text_count=5203431 --qpi=10 --iter=0 --e_value=0.00000000001
--- text-reuse/text_reuse_blast_batches_test3.sh
+++ text-reuse/text_reuse_blast_batches_test3.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=256G
+#SBATCH --mem=16G
 #SBATCH --output=std.out
 #SBATCH --error=std.err
 #SBATCH --mail-type=ALL
@@ -14,10 +14,7 @@
 
 cd /scratch/project_2000230/txt_reuse/blast_ecco/
 module load python-data/3.7.3-1
-rm -r venv
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt --user
 cd /scratch/project_2000230/txt_reuse/blast_ecco/code/work
 export PATH="/scratch/project_2000230/txt_reuse/ncbi-blast-2.5.0+-src/c++/ReleaseMT/bin:$PATH"
-srun python blast_batches.py  --output_folder="/scratch/project_2000230/txt_reuse/blast_work/" --batch_folder="/scratch/project_2000230/txt_reuse/blast_work/data_out_e0_00000000001_qpi100_i0_threads4" --threads=4 --text_count=5203431 --qpi=100 --iter=0 --e_value=0.00000000001
+srun python blast_batches.py  --output_folder="/scratch/project_2000230/txt_reuse/blast_work_full_testsets/set3" --batch_folder="/scratch/project_2000230/txt_reuse/blast_work_full_testsets/set3/data_out_e0_0000000000001_qpi10_i0_threads4" --threads=4 --text_count=5203431 --qpi=10 --iter=0 --e_value=0.0000000000001
