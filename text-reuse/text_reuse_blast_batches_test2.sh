54fa08080366fae4e4c4f19b872ede8eea8557b7
Ville Vaara <villepvaara@gmail.com>
2019-11-27 16:54:55 +0200

    e_value tests

--- text-reuse/text_reuse_blast_batches_test2.sh
+++ text-reuse/text_reuse_blast_batches_test2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=TR_bb_t2
+#SBATCH --job-name=TR_bb_t2_e8
 #SBATCH --account=Project_2000230
 #SBATCH --partition=small
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=10G
+#SBATCH --gres=nvme:280
 #SBATCH --output=std_t2.out
 #SBATCH --error=std_t2.err
 #SBATCH --mail-type=ALL
@@ -17,4 +18,14 @@ module load python-data/3.7.3-1
 pip install -r requirements.txt --user
 cd /scratch/project_2000230/txt_reuse/blast_ecco/code/work
 export PATH="/scratch/project_2000230/txt_reuse/ncbi-blast-2.5.0+-src/c++/ReleaseMT/bin:$PATH"
-srun python blast_batches.py  --output_folder="/scratch/project_2000230/txt_reuse/blast_work_full_testsets/set2" --batch_folder="/scratch/project_2000230/txt_reuse/blast_work_full_testsets/set2/data_out_e0_00000000001_qpi10_i0_threads4" --threads=4 --text_count=5203431 --qpi=10 --iter=0 --e_value=0.00000000001
+
+echo "$(date)" " - Copying data to LOCAL_SCRATCH"
+scp -r /scratch/project_2000230/txt_reuse/blast_work $LOCAL_SCRATCH
+echo "$(date)" " - Copying done to LOCAL_SCRATCH"
+srun python blast_batches.py  --output_folder="$LOCAL_SCRATCH/blast_work" --batch_folder="$LOCAL_SCRATCH/blast_work/data_out" --threads=20 --text_count=5203431 --qpi=10 --iter=0 --e_value=0.000000001
+echo "blast conf: --threads=20 --text_count=5203431 --qpi=10 --iter=0 --e_value=0.000000001"
+echo "$(date)" " - Python script done. Copying results."
+cd /scratch/project_2000230/txt_reuse/blast_work_full_testsets_results
+mkdir set2
+scp -r $LOCAL_SCRATCH/blast_work/data_out /scratch/project_2000230/txt_reuse/blast_work_full_testsets_results/set2
+echo "$(date)" " - Job finished."
