2a9cc97c58aeaac82cdd459b6291c9420c6fa188
Ville Vaara <villepvaara@gmail.com>
2019-11-28 15:31:49 +0200

    Adjust data preparer.

--- text-reuse/text_reuse_data_preparer.sh
+++ text-reuse/text_reuse_data_preparer.sh
@@ -7,8 +7,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=128G
-#SBATCH --output=std.out
-#SBATCH --error=std.err
+#SBATCH --output=std_data_prep.out
+#SBATCH --error=std_data_prep.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=villepvaara@gmail.com
 
