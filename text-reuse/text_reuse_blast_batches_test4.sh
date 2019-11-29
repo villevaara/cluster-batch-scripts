9805ba00c39196444ef0698e160fdec5d0795803
Ville Vaara <villepvaara@gmail.com>
2019-11-29 17:12:52 +0200

    Update test scripts to higher qpi and memory.

--- text-reuse/text_reuse_blast_batches_test4.sh
+++ text-reuse/text_reuse_blast_batches_test4.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=TR_bb_t4_e14
+#SBATCH --job-name=TR_bb_t4_e-15
 #SBATCH --account=Project_2000230
 #SBATCH --partition=small
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=10G
+#SBATCH --mem=20G
 #SBATCH --gres=nvme:280
-#SBATCH --output=std_t4.out
-#SBATCH --error=std_t4.err
+#SBATCH --output=std_t1.out
+#SBATCH --error=std_t1.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=villepvaara@gmail.com
 
@@ -22,8 +22,8 @@ export PATH="/scratch/project_2000230/txt_reuse/ncbi-blast-2.5.0+-src/c++/Releas
 echo "SHELLSCRIPT - $(date) - Copying data to LOCAL_SCRATCH"
 scp -r /scratch/project_2000230/txt_reuse/blast_work $LOCAL_SCRATCH
 echo "SHELLSCRIPT - $(date) - Copying done to LOCAL_SCRATCH"
-srun python blast_batches.py  --output_folder="$LOCAL_SCRATCH/blast_work" --batch_folder="$LOCAL_SCRATCH/blast_work/data_out" --threads=20 --text_count=5203431 --qpi=10 --iter=0 --e_value=0.000000000000001
-echo "BLAST CONF: --threads=20 --text_count=5203431 --qpi=10 --iter=0 --e_value=0.000000000000001"
+srun python blast_batches.py  --output_folder="$LOCAL_SCRATCH/blast_work" --batch_folder="$LOCAL_SCRATCH/blast_work/data_out" --threads=20 --text_count=5203431 --qpi=100 --iter=0 --e_value=0.000000000000001
+echo "BLAST CONF: --threads=20 --text_count=5203431 --qpi=100 --iter=0 --e_value=0.000000000000001"
 echo "SHELLSCRIPT - $(date) - Python script done. Copying results."
 cd /scratch/project_2000230/txt_reuse/blast_work_full_testsets_results
 mkdir set4
---
+++ text-reuse/text_reuse_interactive.sh
@@ -0,0 +1,1 @@
+srun --ntasks=1 --time=04:00:00 --mem=10G --cpus-per-task=10 --gres=nvme:280 --pty --account=Project_2000230 --partition=small --mail-type=BEGIN --mail-user=villepvaara@gmail.com bash
