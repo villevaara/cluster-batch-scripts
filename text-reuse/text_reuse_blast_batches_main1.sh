a44c600abf2d21252dfb7cb324372ffef15367d7
Ville Vaara <villepvaara@gmail.com>
2019-12-09 01:28:20 +0200

    Test with 40 cores

--- text-reuse/text_reuse_blast_batches_main1.sh
+++ text-reuse/text_reuse_blast_batches_main1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=TR_bb_t1_e-9
+#SBATCH --job-name=TR_bb_t4_e-15
 #SBATCH --account=Project_2000230
 #SBATCH --partition=small
 #SBATCH --time=72:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=20
 #SBATCH --mem=12G
 #SBATCH --gres=nvme:280
 #SBATCH --output=std_t1.out
@@ -22,10 +22,10 @@ export PATH="/scratch/project_2000230/txt_reuse/ncbi-blast-2.5.0+-src/c++/Releas
 echo "SHELLSCRIPT - $(date) - Copying data to LOCAL_SCRATCH"
 scp -r /scratch/project_2000230/txt_reuse/blast_work $LOCAL_SCRATCH
 echo "SHELLSCRIPT - $(date) - Copying done to LOCAL_SCRATCH"
-srun python blast_batches.py --output_folder="$LOCAL_SCRATCH/blast_work" --batch_folder="$LOCAL_SCRATCH/blast_work/data_out" --threads=40 --text_count=5203431 --qpi=100 --iter=0 --e_value=0.000000001
-echo "BLAST CONF: --threads=20 --text_count=5203431 --qpi=100 --iter=0 --e_value=0.000000001"
+srun python blast_batches.py  --output_folder="$LOCAL_SCRATCH/blast_work" --batch_folder="$LOCAL_SCRATCH/blast_work/data_out" --threads=20 --text_count=5203431 --qpi=100 --iter=0 --e_value=0.000000000000001
+echo "BLAST CONF: --threads=20 --text_count=5203431 --qpi=100 --iter=0 --e_value=0.000000000000001"
 echo "SHELLSCRIPT - $(date) - Python script done. Copying results."
 cd /scratch/project_2000230/txt_reuse/blast_work_full_testsets_results
-mkdir set1
-scp -r $LOCAL_SCRATCH/blast_work/data_out /scratch/project_2000230/txt_reuse/blast_work_full_testsets_results/set1
+mkdir set4
+scp -r $LOCAL_SCRATCH/blast_work/data_out /scratch/project_2000230/txt_reuse/blast_work_full_testsets_results/set4
 echo "SHELLSCRIPT - $(date) - Job finished."
