1c212bc7fbedcb90665e7395e982c4a5987e909f
Ville Vaara <villepvaara@gmail.com>
2020-02-10 16:02:40 +0200

    shortened job time

--- text-reuse/text_reuse_blast_batches_main_small_range_cores.sh
+++ text-reuse/text_reuse_blast_batches_main_small_range_cores.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=TR_range
 #SBATCH --account=Project_2000230
 #SBATCH --partition=small
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
