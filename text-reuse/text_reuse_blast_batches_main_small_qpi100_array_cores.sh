e337267df50ef894410784359501c0ff6e6618a4
Ville Vaara <villepvaara@gmail.com>
2020-02-12 14:47:20 +0200

    qpi100 after all

---
+++ text-reuse/text_reuse_blast_batches_main_small_qpi100_array_cores.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+#SBATCH --job-name=TR_range
+#SBATCH --account=Project_2000230
+#SBATCH --partition=small
+#SBATCH --time=4:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=16G
+#SBATCH --array=1-50
+#SBATCH --gres=nvme:280
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=villepvaara@gmail.com
+
+#SBATCH --output=logs/tr_range_%j.out
+#SBATCH --error=logs/err/tr_range_%j.err
+# note: For array jobs %A is job ID, %a is array index. For normal jobs %j is job ID.
+
+# created: Feb 3, 2020
+# author: Ville Vaara
+
+# usage:
+# You can pass an argument after the script as if you were running it directly on the shell like this:
+# sbatch text_reuse_blast_batches_main_small_generic.sh [firstindex] [lastindex] [cores]
+# eg.:
+# sbatch text_reuse_blast_batches_main_small_generic.sh 700 709 20
+# And then the arguments will be available inside the shell script as $1 $2 $3
+
+cd /scratch/project_2000230/txt_reuse/blast_ecco/
+module load python-data/3.7.3-1
+pip install -r requirements.txt --user
+cd /scratch/project_2000230/txt_reuse/blast_ecco/code/work
+
+export LMDB_PATH="$HOME/localinstall/usr/local"
+export PATH="$PATH:$HOME/localinstall/usr/local/bin"
+export PATH="$HOME/customblast/ncbi-blast-2.6.0+-src/c++/ReleaseMT/bin:$PATH"
+
+echo "SHELLSCRIPT - $(date) - Copying data to LOCAL_SCRATCH"
+srun scp -r /scratch/project_2000230/txt_reuse/blast_work $LOCAL_SCRATCH
+echo "SHELLSCRIPT - $(date) - Copying done to LOCAL_SCRATCH"
+srun python blast_batches.py --output_folder="$LOCAL_SCRATCH/blast_work" --batch_folder="$LOCAL_SCRATCH/blast_work/data_out" --threads=$1 --text_count=1302141 --qpi=100 --iter=${SLURM_ARRAY_TASK_ID} --e_value=0.000000001
+echo "SHELLSCRIPT - Finished iter ${SLURM_ARRAY_TASK_ID}."
+echo "SHELLSCRIPT - $(date) - Rsync results."
+srun rsync -r $LOCAL_SCRATCH/blast_work/data_out/* /scratch/project_2000230/txt_reuse/results_qpi100
+echo "SHELLSCRIPT - $(date) - Job finished."
+blastp -version
---
+++ text-reuse/tr_small_array_qpi100_cores.sh
@@ -0,0 +1,55 @@
+# takes three parameters
+# -c [number of cores to use]
+# -s [first iteration]
+# -e [last iteration]
+
+usage()
+{
+    echo "usage: tr_small_array_cores [-f cores] [-s start] [-e end]"
+}
+
+cores=10000
+start=10000
+end=10000
+error=0
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -c | --cores )          shift
+                                cores=$1
+                                ;;
+        -s | --start )          shift
+                                start=$1
+                                ;;
+        -e | --end )            shift
+                                end=$1
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+if [ $cores -eq 10000 ]
+then
+    echo "Set cores (-c | --cores) to value between 1-40."
+    error=1
+fi
+if [ $start -eq 10000 ]
+then
+    echo "Set start (-s | --start) value."
+    error=1
+fi
+if [ $end -eq 10000 ]
+then
+    echo "Set end (-e | --end) value."
+    error=1
+fi
+if [ $error -eq 1 ]
+then
+    exit 1
+fi
+
+echo "tr small i$start-$end cores: $cores"
+
+sbatch --array=$start-$end --job-name=tr_i$start-$end --output=logs/tr_i$start-$end_%a_%A.out --error=logs/err/tr_i$start-$end_%a_%A.err --cpus-per-task=$cores text_reuse_blast_batches_main_small_qpi100_array_cores.sh $cores
