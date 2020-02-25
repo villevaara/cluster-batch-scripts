3f9dd50f2fb7f367f91a46b2032938c78f7b90b2
Ville Vaara <villepvaara@gmail.com>
2020-02-25 12:16:56 +0200

    fix stupid bug

--- text-reuse/text_reuse_blast_batches_main_small_qpi100_arrayrange_cores.sh
+++ text-reuse/text_reuse_blast_batches_main_small_qpi100_arrayrange_cores.sh
@@ -39,7 +39,7 @@ export LMDB_PATH="$HOME/localinstall/usr/local"
 export PATH="$PATH:$HOME/localinstall/usr/local/bin"
 export PATH="$HOME/customblast/ncbi-blast-2.6.0+-src/c++/ReleaseMT/bin:$PATH"
 
-startiter=$((${SLURM_ARRAY_TASK_ID}*10+$addition))
+startiter=$((${SLURM_ARRAY_TASK_ID}*10+$2))
 
 echo "SHELLSCRIPT - $(date) - Copying data to LOCAL_SCRATCH"
 srun scp -r /scratch/project_2000230/txt_reuse/blast_work $LOCAL_SCRATCH
