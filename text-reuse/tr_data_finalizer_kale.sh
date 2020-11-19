f4538bcab119f60fa4c094d02bf95571dc8935ce
Ville Vaara <villepvaara@gmail.com>
2020-11-19 15:26:58 +0200

    Fix bug

--- text-reuse/tr_data_finalizer_kale.sh
+++ text-reuse/tr_data_finalizer_kale.sh
@@ -21,7 +21,7 @@ mkdir -p /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}/db/origina
 srun rsync -rv --update /wrk/users/vvaara/txt_reuse/blast_work_from_puhti/blast_work/db/original_data_DB/* /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}/db/original_data_DB
 
 echo "SHELLSCRIPT - $(date) - Starting data finalizer."
-srun python generate_json_multiprocess_lmdb.py --datadir "/wrk/users/vvaara/txt_reuse/results_qpi100_parts/${SLURM_ARRAY_TASK_ID}" --outdir "/wrk/users/vvaara/txt_reuse/results_qpi100_filled" --threads 40 --db "/wrk/users/vvaara/txt_reuse/blast_work_from_puhti/blast_work/db/original_data_DB"
+srun python generate_json_multiprocess_lmdb.py --datadir "/wrk/users/vvaara/txt_reuse/results_qpi100_parts/${SLURM_ARRAY_TASK_ID}" --outdir "/wrk/users/vvaara/txt_reuse/results_qpi100_filled" --threads 40 --db "/wrk/users/vvaara/txt_reuse/blast_work_from_puhti/blast_work/db/original_data_DB/* /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}/db/original_data_DB"
 
 echo "SHELLSCRIPT - $(date) - DONE - Removing temp directory."
 rm -r /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}
