f4446e1bde7365cbe7fb8cef1bc4d09aee89ac12
Ville Vaara <villepvaara@gmail.com>
2020-11-19 14:36:34 +0200

    Adjust kale script

--- text-reuse/tr_data_finalizer_kale.sh
+++ text-reuse/tr_data_finalizer_kale.sh
@@ -16,5 +16,12 @@ cd $USERAPPL/data_input_text_reuse/code
 module load Python/3.6.6-intel-2018b
 pip install -r requirements.txt --user
 
+echo "SHELLSCRIPT - $(date) - Copying data to temp directory."
+mkdir -p /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}/db/original_data_DB
+rsync -rv --update /wrk/users/vvaara/txt_reuse/blast_work_from_puhti/blast_work/db/original_data_DB/* /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}/db/original_data_DB
+
 echo "SHELLSCRIPT - $(date) - Starting data finalizer."
 srun python generate_json_multiprocess_lmdb.py --datadir "/wrk/users/vvaara/txt_reuse/results_qpi100_parts/${SLURM_ARRAY_TASK_ID}" --outdir "/wrk/users/vvaara/txt_reuse/results_qpi100_filled" --threads 40 --db "/wrk/users/vvaara/txt_reuse/blast_work_from_puhti/blast_work/db/original_data_DB"
+
+echo "SHELLSCRIPT - $(date) - DONE - Removing temp directory."
+rm -r /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}
