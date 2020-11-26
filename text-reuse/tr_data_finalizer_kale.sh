51406cfa1382168787a9cff3a99ce32694d0f836
Ville Vaara <villepvaara@gmail.com>
2020-11-26 13:16:35 +0200

    Adjust for optimized script.

--- text-reuse/tr_data_finalizer_kale.sh
+++ text-reuse/tr_data_finalizer_kale.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=8G
+#SBATCH --mem-per-cpu=100MB
 #SBATCH --array=1-9
 #SBATCH --output=logs/tr_data_final_%A_%a.out
 #SBATCH --error=logs/err/tr_data_final_%A_%a.err
