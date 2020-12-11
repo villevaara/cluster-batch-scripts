c2c2731fa657ba1e01af7f67950940bf4a5f9cf4
Ville Vaara <villepvaara@gmail.com>
2020-12-12 01:47:28 +0200

    Adjust kale script

--- text-reuse/tr_data_finalizer_kale.sh
+++ text-reuse/tr_data_finalizer_kale.sh
@@ -6,7 +6,6 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=400MB
-#SBATCH --array=1-9
 #SBATCH --output=logs/tr_data_final_%A_%a.out
 #SBATCH --error=logs/err/tr_data_final_%A_%a.err
 #SBATCH --mail-type=ALL
@@ -17,11 +16,11 @@ module load Python/3.6.6-intel-2018b
 pip install -r requirements.txt --user
 
 echo "SHELLSCRIPT - $(date) - Copying data to temp directory."
-mkdir -p /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}/db/original_data_DB
-srun rsync -rv --update /wrk/users/vvaara/txt_reuse/blast_work_from_puhti/blast_work/db/original_data_DB/* /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}/db/original_data_DB
+mkdir -p /wrk/users/vvaara/txt_reuse/tempdb/db/original_data_DB
+srun rsync -rv --update /wrk/users/vvaara/txt_reuse/blast_work_from_puhti/blast_work/db/original_data_DB/* /wrk/users/vvaara/txt_reuse/tempdb/db/original_data_DB
 
 echo "SHELLSCRIPT - $(date) - Starting data finalizer."
-srun python generate_json_multiprocess_lmdb.py --datadir "/wrk/users/vvaara/txt_reuse/results_qpi100_parts/${SLURM_ARRAY_TASK_ID}" --outdir "/wrk/users/vvaara/txt_reuse/results_qpi100_filled" --threads ${SLURM_CPUS_PER_TASK} --db "/wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}/db/original_data_DB"
+srun python generate_json_multiprocess_lmdb.py --datadir "/wrk/users/vvaara/txt_reuse/results_qpi100/" --outdir "/wrk/users/vvaara/txt_reuse/results_qpi100_remainder" --threads ${SLURM_CPUS_PER_TASK} --db "/wrk/users/vvaara/txt_reuse/tempdb/db/original_data_DB"
 
 echo "SHELLSCRIPT - $(date) - DONE - Removing temp directory."
-srun rm -r /wrk/users/vvaara/txt_reuse/temp/array${SLURM_ARRAY_TASK_ID}
+srun rm -r /wrk/users/vvaara/txt_reuse/tempdb
