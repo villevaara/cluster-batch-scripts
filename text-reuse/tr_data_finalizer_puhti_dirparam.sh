0771b27ae93d26738741682cd4b2dbac0973fd65
Ville Vaara <villepvaara@gmail.com>
2021-01-25 11:33:42 +0200

    changed logic

--- text-reuse/tr_data_finalizer_puhti_dirparam.sh
+++ text-reuse/tr_data_finalizer_puhti_dirparam.sh
@@ -7,12 +7,15 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=18G
-#SBATCH --output=logs/tr_data_final_%A_%a.out
-#SBATCH --error=logs/err/tr_data_final_%A_%a.err
+#SBATCH --output=logs/tr_data_final_%j_$1.out
+#SBATCH --error=logs/err/tr_data_final_%j_$1.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=villepvaara@gmail.com
 #SBATCH --gres=nvme:280
 # note: For array jobs %A is job ID, %a is array index. For normal jobs %j is job ID.
+
+# vars:
+# $1 = subdir
 
 cd $HOME/data_input_text_reuse/code
 module load python-data/3.7.3-1
@@ -23,4 +26,4 @@ echo "SHELLSCRIPT - $(date) - Starting data finalizer."
 echo "SHELLSCRIPT - $(date) - Copying data to LOCAL_SCRATCH"
 scp -r /scratch/project_2000230/txt_reuse/blast_work $LOCAL_SCRATCH
 echo "SHELLSCRIPT - $(date) - Copying done to LOCAL_SCRATCH"
-srun python generate_json_multiprocess_lmdb.py --datadir "/scratch/project_2000230/txt_reuse/results_qpi100_parts/${SLURM_ARRAY_TASK_ID}" --outdir "/scratch/project_2000230/txt_reuse/results_qpi100_filled" --threads ${SLURM_CPUS_PER_TASK} --db "$LOCAL_SCRATCH/blast_work/db/original_data_DB"
+srun python generate_json_multiprocess_lmdb.py --datadir "/scratch/project_2000230/txt_reuse/results_qpi100_parts/$1" --outdir "/scratch/project_2000230/txt_reuse/results_qpi100_filled" --threads ${SLURM_CPUS_PER_TASK} --db "$LOCAL_SCRATCH/blast_work/db/original_data_DB"
--- text-reuse/tr_dataf_wrapper.sh
+++
@@ -1,2 +0,0 @@
-echo "Array: $1"
-sbatch --array=$1 tr_data_finalizer_puhti_wrapped.sh
